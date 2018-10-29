c9ec2543b71683ba37776aff04d08a36844a5067
Justin Weigle <jw6858@cs.ship.edu>
2018-10-29 14:45:47 -0400

    Constants and DesktopLauncher
    added code for Constants and DesktopLauncher from Chapter 4

--- desktop/src/com/libgdx/zeppelinraiders/desktop/DesktopLauncher.java
+++ desktop/src/com/libgdx/zeppelinraiders/desktop/DesktopLauncher.java
@@ -2,11 +2,32 @@ package com.libgdx.zeppelinraiders.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker.Settings;
 import com.libgdx.zeppelinraiders.ZeppelinRaidersMain;
 
 public class DesktopLauncher {
+	private static boolean rebuildAtlas = true;
+	private static boolean drawDebugOutline = true;
+	
 	public static void main (String[] arg) {
+		//rebuilds texture atlas every time the game is run on desktop
+		if(rebuildAtlas)
+		{
+			Settings settings = new Settings();
+			settings.maxWidth = 1024;
+			settings.maxHeight = 1024;
+			settings.duplicatePadding = false;
+			settings.debug = drawDebugOutline;
+			TexturePacker.process(settings, "assets-raw/images", "../core/assets/images",
+					"canyonbunny.pack");
+		}
+				
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "ZeppelinRaiders";
+		config.width = 800;
+		config.height = 480;
+		
 		new LwjglApplication(new ZeppelinRaidersMain(), config);
 	}
 }
